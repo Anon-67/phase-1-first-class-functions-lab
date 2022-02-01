5387dbd75530dd8f5b56bd5e8e0860622f20325b
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-01 17:52:47 -0600

    commit tree f729554cc2a3cf727131c0ef57a136d7696d33c8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function (array) {
+    return array.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (array) {
+    return array.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    return function (fare) {
+        return fare * x;
+    }
+}
+
+
+const fareDoubler =  createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, cb) {
+    return cb(drivers);
+}
